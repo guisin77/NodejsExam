3a2ee83c2866b45f97823a120ba87cce0158b88c
guisin77 <guisin77@gmail.com>
2017-09-29 17:30:11 +0900

    Node RestFul

---
+++ restfulExam/rest.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+var users = [];
+
+app.get('/user' , function(req,res){
+	res.send(JSON.stringify(users))
+})
+
+app.get('/user/:id', function(req,res){
+	var select_index = -1;
+	for(var i=0;i<users.length;i++) {
+		var obj = users[i];
+		if (obj.id ==Number(req.params.id)) {
+			select_index = i;
+			break;
+		}
+	}
+
+	if(select_index==-1) {
+		res.send(JSON.stringify({}));
+	} else {
+		res.send(JSON.stringify(users[select_index]));
+	}
+
+	res.send(JSON.stringify({api:'get user info' ,
+                              id: req.params.id}));
+})
+
+app.post('/user', function(req,res){
+	var name = req.body.name;
+	var age  = Number(req.body.age);
+	var obj = {id:users.length +1 ,  name:name , age:age }
+	users.push(obj);
+	res.send(JSON.stringify({result:true ,
+		                        api:'add user info'  }));
+})
+
+app.put('/user/:id', function(req,res){
+
+	var select_index = -1;
+	for(var i=0;i<users.length;i++) {
+		var obj = users[i];
+		if (obj.id ==Number(req.params.id)) {
+			select_index = i;
+			break;
+		}
+	}
+
+	if(select_index==-1) {
+		res.send(JSON.stringify({result:false}));
+	} else {
+		
+		var name = req.body.name;
+		var age  = Number(req.body.age);
+		var obj =  Number(req.body.id);
+		users[select_index] = obj ;
+		res.send(JSON.stringify({result:true ,
+			                        api:'modify user info'  }));
+	}
+	
+ 
+})
+
+app.delete('/user/:id', function(req,res){
+
+	var select_index = -1;
+	for(var i=0;i<users.length;i++) {
+		var obj = users[i];
+		if (obj.id ==Number(req.params.id)) {
+			select_index = i;
+			break;
+		}
+	}
+
+	if(select_index==-1) {
+		res.send(JSON.stringify({result:false}));
+	} else {
+		users.splice(select_index, 1);
+		res.send(JSON.stringify({result:true}));
+	}
+
+})
+
+app.listen(52273, function(){
+	console.log('server running');
+})
